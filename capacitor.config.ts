d33db15043c0a8f46784681260696fc1547c8c8b
yunisaaaa/student-assignment-tracker <mojicalianeunice17@gmail.com>
2026-09-15 22:40:39 +0800

    Tracker

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'student-assignment-tracker',
+  appName: 'Student Assignment Tracker',
   webDir: 'dist'
 };
 
